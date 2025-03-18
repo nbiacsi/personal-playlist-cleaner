a1e9fa8ea10956f6dadd595de8a484d278a3a13f
SlothArms <60442611+nbiacsi@users.noreply.github.com>
2025-03-17 23:06:09 -0400

    Fixed remove_songs if statement, added docstrings

--- playlist_cleaner.py
+++ playlist_cleaner.py
@@ -1,37 +1,53 @@
-"""
+'''
     Author: Sloth
     Date: 9/16/2024
     Description: Python script to get all songs in Hype playlist and remove all songs that aren't liked songs.
-"""
+'''
 
+from dotenv import load_dotenv
 import spotipy
 import spotipy.util as util
-from dotenv import load_dotenv
 
 import os
+from typing import Any
 
 
 load_dotenv()
 
 
-def remove_songs(
-    sp: spotipy.Spotify,
-    liked_songs: list[str],
-    playlist_songs: list[str],
-    playlist_id: str,
-) -> None:
-    songs: list[str] = []
-    for song in playlist_songs:
-        if song not in liked_songs:
-            songs.append(song)
+def remove_songs(sp: spotipy.Spotify, liked_songs: list[str], playlist_songs: list[str], playlist_id: str) -> None:
+    '''
+    Removes songs that are in the Hype playlist that are not liked songs.
 
-    if songs != None:
+    Args:
+        sp (spotipy.Spotify): Spotipy authentication object.
+        liked_songs (list[str]): List of liked song IDs.
+        playlist_songs (list[str]): List of Hype song IDs.
+        playlist_id (str): ID of the Hype playlist.
+
+    Returns:
+        None.
+    '''
+
+    songs: list[str] = [song for song in playlist_songs if song not in liked_songs]
+    if len(songs) > 0:
         sp.playlist_remove_all_occurrences_of_items(
             playlist_id, songs, snapshot_id=None
         )
 
 
 def get_hypesongs(sp: spotipy.Spotify, playlist_id: str) -> list[str]:
+    '''
+    Gets IDs of all Hype playlist songs.
+
+    Args:
+        sp (spotipy.Spotify): Spotipy authentication object.
+        playlist_id (str): ID of the Hype playlist.
+
+    Returns:
+        list[str]: Hype song IDs.
+    '''
+
     songs: list[str] = []
     i: int = 0
     while True:
@@ -40,18 +56,27 @@ def get_hypesongs(sp: spotipy.Spotify, playlist_id: str) -> list[str]:
         else:
             response = sp.next(response)
 
-        for song in response["items"]:
-            songs.append(song["track"]["id"])
+        for song in response['items']:
+            songs.append(song['track']['id'])
 
-        if response["next"] == None:
+        if response['next'] == None:
             return songs
 
         i += 1
 
 
-def get_likedsongs_response(
-    sp: spotipy.Spotify, liked_songs_count: int
-) -> dict[str, str]:
+def get_likedsongs_response(sp: spotipy.Spotify, liked_songs_count: int) -> dict[str, Any]:
+    '''
+    Gets the total response object of all liked songs.
+
+    Args:
+        sp (spotipy.Spotify): Spotipy authentication object.
+        liked_songs_count (int): Count of liked songs.
+
+    Returns:
+        dict[str, Any]: Response object of all liked songs.
+    '''
+
     response: dict[str, str] = {}
     i: int = 0
     limit: int = 50
@@ -68,16 +93,26 @@ def get_likedsongs_response(
 
 
 def get_likedsongs(sp: spotipy.Spotify) -> list[str]:
-    liked_songs_count = sp.current_user_saved_tracks()["total"]
+    '''
+    Gets all IDs of liked songs.
+
+    Args:
+        sp (spotipy.Spotify): Spotipy authentication object.
+
+    Returns:
+        list[str]: Liked song IDs
+    '''
+
+    liked_songs_count = sp.current_user_saved_tracks()['total']
     response: dict[str, str] = get_likedsongs_response(sp, liked_songs_count)
     liked_songs: list[str] = []
     i: int = 0
     blob: int = 0
 
     while i < liked_songs_count and blob < liked_songs_count:
-        songs: list[dict[str, str]] = response[blob]["items"]
+        songs: list[dict[str, str]] = response[blob]['items']
         for j in range(len(songs)):
-            liked_songs.append(songs[j]["track"]["id"])
+            liked_songs.append(songs[j]['track']['id'])
             i += 1
 
         blob += 50
@@ -86,16 +121,36 @@ def get_likedsongs(sp: spotipy.Spotify) -> list[str]:
 
 
 def get_playlist_id(sp: spotipy.Spotify) -> str:
-    id: str = sp.me()["id"]
-    return sp.user_playlists(id)["items"][0]["id"]
+    '''
+    Gets the Hype playlist ID to remove songs from.
+
+    Args:
+        sp (spotipy.Spotify): Spotipy authentication object.
+
+    Returns:
+        str: Hype playlist ID.
+    '''
+
+    id: str = sp.me()['id']
+    return sp.user_playlists(id)['items'][0]['id']
 
 
 def authorize() -> spotipy.Spotify:
-    client_id: str = os.getenv("CLIENT_ID")
-    client_secret: str = os.getenv("CLIENT_SECRET")
-    redirect_uri: str = "http://localhost:7777/callback"
-    scope: str = "user-library-read playlist-modify-public"
-    username: str = "nickbiacsi@gmail.com"
+    '''
+    Gets a spotipy.Spotify authentication object used to make calls to the spotipy module.
+
+    Args:
+        None.
+
+    Returns:
+        spotipy.Spotify: Spotipy authentication object.
+    '''
+
+    client_id: str = os.getenv('CLIENT_ID')
+    client_secret: str = os.getenv('CLIENT_SECRET')
+    redirect_uri: str = 'http://localhost:7777/callback'
+    scope: str = 'user-library-read playlist-modify-public'
+    username: str = 'nickbiacsi@gmail.com'
 
     token: str = util.prompt_for_user_token(
         username,
@@ -116,5 +171,5 @@ def main() -> None:
     remove_songs(sp, liked_songs, hype_songs, playlist_id)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
